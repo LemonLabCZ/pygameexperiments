a98330cae9f0584707dcab725aa80dfe8e6ff514
Lukáš Hejtmánek <hejtmy@gmail.com>
2024-06-30 13:44:23 +0200

    script to generate settings for the syllable comparison

---
+++ scripts/syllable_comparison/generate_settings.py
@@ -0,0 +1,11 @@
+import random
+from src.syllable_comparison import settings_generation as generator
+random.seed(2024)
+
+df_trials = generator.create_experiment_trials()
+df_trials.head()
+df_trials.to_csv("settings/syllable_comparison/settings1.csv")
+
+for i in range(1, 100):
+    df_trials = generator.create_experiment_trials(i)
+    df_trials.to_csv(generator.generate_settings_filename(i))
