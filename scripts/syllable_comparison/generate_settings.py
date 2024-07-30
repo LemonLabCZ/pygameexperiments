c06e58d9e5672a6f15c28d59bafb761df6a54442
Ivana Kvapilikova <ivana.kvapilikova@borndigital.ai>
2024-07-30 07:52:47 +0200

    Change order of stimuli to control pauses between deviants

--- scripts/syllable_comparison/generate_settings.py
+++ scripts/syllable_comparison/generate_settings.py
@@ -1,5 +1,6 @@
 import os
 from src.syllable_comparison import settings_generation as generator
+from collections import defaultdict
 
 os.makedirs(generator.settings_folder(), exist_ok=True)
 
@@ -8,5 +9,6 @@ df_trials.head()
 df_trials.to_csv(generator.generate_settings_filename(1))
 
 for i in range(1, 100):
+    generator.POOL = defaultdict(list)
     df_trials = generator.create_experiment_trials(i)
     df_trials.to_csv(generator.generate_settings_filename(i))
