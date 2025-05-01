f3cdc8eecc193e9b5beab1a9ee0551db3f5af1ce
Lukáš Hejtmánek <hejtmy@gmail.com>
2025-05-01 15:26:41 +0200

    Updates show text and screen size functions

--- src/utils.py
+++ src/utils.py
@@ -13,8 +13,18 @@ def initScreen(screenSize, screenColor):
     return screen
 
 
-def getScreenSize():
-    sid = ''
-    ROOT = tk.Tk()
-    ROOT.withdraw()
-    return (ROOT.winfo_screenwidth(),ROOT.winfo_screenheight())
+def show_text(screen, text, font_size, color):
+    if screen is None:
+        raise ValueError("Screen is not initialized")
+    screen_size = get_screen_size(screen)
+    center = (screen_size[0] / 2, screen_size[1] / 2)
+    font = pygame.font.Font("freesansbold.ttf", font_size)
+    text = font.render(text, True, color)
+    screen.blit(text, (center[0] - text.get_width()/2, center[1] - text.get_height()/2))
+    return screen
+
+
+def get_screen_size(screen: pygame.Surface):
+    if screen is None:
+        raise ValueError("Screen is not initialized")
+    return (screen.get_width(), screen.get_height())
